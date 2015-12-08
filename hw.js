eb9638eb301e5554c1df7a9d5d87b0f57d881444
Kira Melamed <kiramelamed@hotmail.com>
2015-12-08 13:47:38 -0500

    Homework submission

---
+++ hw.js
@@ -0,0 +1,17 @@
+// 1. Create new variable called name and assign it a string with your name in it.
+
+var name = "Kira"
+
+// 2. Write a function that takes a number, adds 2 to it, subtracts 5 from it, then returns a value.
+
+function math(someNumber) {
+	return someNumber + 2 - 5;
+}; 
+
+console.log(math(10));
+
+// 3. Create a for loop that counts from 1 to 10.
+
+for(var i = 0; i<=10; i++){   
+	console.log(i);
+}
